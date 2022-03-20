88030b04d4851121bc1195c69e14316266fff04f
Carlos Leal <cbrito-l@student.42.fr>
2022-03-20 18:08:35 +0000

    v1.0

---
+++ include/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbrito-l <cbrito-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 17:59:01 by cbrito-l          #+#    #+#             */
+/*   Updated: 2022/03/20 17:59:01 by cbrito-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
