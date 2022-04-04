8df87b3a54a0cbc826895dc46982263f978b0236
Carlos Leal <carlos.leal.92@gmail.com>
2022-04-04 10:54:00 +0100

    structure for parse

--- src/parse.c
+++ src/parse.c
@@ -14,4 +14,41 @@
 
 void parse_args(char *str)
 {
+	verify pipes '|'
+	/* cmd1 | cmd2 | ... 
+		if they exists and if so, how many
+	*/
+
+	verify end cmd ';'
+	/* cmd1; cmd2; ... 
+		if they exists and if so, how many
+	*/
+	
+	verify redirections '<'/'>'/">>"
+	/* program < file 
+	   cmd1 > file
+	   cmd1 >> file
+	*/
+
+	split input
+	/* split the initial str into small tokens */
+
+	verify cmd (echo, pwd, cd, ...)
+	/* search for the command */
+	
+	verify ''/""
+	/* verify is '' or "" exists and if its close */
+	
+	verify input according to each cmd
+	/*      echo str/"str"/-n str/-n "str" 
+		cd str/./../~    str=(path)
+		pwd (no args)
+		export no args/str   str=(name=value)
+		unset str      str=(name)
+		env (no args)
+		exit (no args)
+	*/
+
 }
+
+
